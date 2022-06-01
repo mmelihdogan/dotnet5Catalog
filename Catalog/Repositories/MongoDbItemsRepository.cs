5dda739a98a79197ee107795ad82e9b9542d0ee6
Melih <42299017+mmelihdogan@users.noreply.github.com>
2022-06-01 17:09:29 +0300

    MongoDB imlementation is finished

---
+++ Catalog/Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Catalog.Entities;
+using Microsoft.VisualBasic;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Catalog.Repositories
+{
+    public class MongoDbItemsRepository : IItemsRepository
+    {
+
+        private const string databaseName = "catalog";
+        
+        private const string collectionName = "items";
+        
+        private readonly IMongoCollection<Item> itemsCollection;
+        
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+
+        public MongoDbItemsRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
+            itemsCollection = database.GetCollection<Item>(collectionName);
+        }
+
+        public void UpdateItem(Item item)
+        {
+            var filter = filterBuilder.Eq(x => x.Id, item.Id);
+            itemsCollection.ReplaceOne(filter, item);
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            return itemsCollection.Find(new BsonDocument()).ToList();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            return itemsCollection.Find(filter).SingleOrDefault();
+        }
+
+        public void CreateItem(Item item)
+        {
+            itemsCollection.InsertOne(item);
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            itemsCollection.DeleteOne(filter);
+        }
+    }
+}
